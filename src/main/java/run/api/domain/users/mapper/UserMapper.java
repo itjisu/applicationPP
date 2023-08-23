6fb560c6dfa3a64eb693d89f492f2f5253ffbd46
itjisu <87115779+itjisu@users.noreply.github.com>
2023-08-23 22:55:47 +0900

    Delete src/main/java/run/api/domain/users directory

--- src/main/java/run/api/domain/users/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package run.api.domain.users.mapper;
-
-import run.api.domain.users.vo.UserVo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper {
-    void saveUser(UserVo userVo);
-}
